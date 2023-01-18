1420a3e4932c2ba9e137fca4108a1800df67e741
Meghan Kolin <123012755+meghankolin@users.noreply.github.com>
2023-01-18 14:48:06 -0500

    Second button added, comment added
    I thought the little button response was cute, so I added a second button to wish you a good day :)

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ var button = document.getElementById("my-btn");
 button.addEventListener("click", function(){
     alert("You clicked me!");
 })
+
+//This is for the happy button I made
+var button = document.getElementById("happy-btn");
+
+button.addEventListener("click", function(){
+    alert("You have clicked on the serotonin button! You get one molecule of serotonin and the button hopes you have a great day :)")
+})
